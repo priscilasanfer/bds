966d3d988bc9e8b947892d234e09a598e84614f8
Priscila Ferreira <priscilasanfer@gmail.com>
2021-09-04 20:56:05 -0300

    Resolução do desafio uri2602 usando JPQL

--- uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,12 +1,12 @@
 package com.devsuperior.uri2602;
 
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
 import com.devsuperior.uri2602.projection.CustomerMinProjection;
 import com.devsuperior.uri2602.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.devsuperior.uri2602.dto.CustomerMinDTO;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,6 +26,14 @@ public class Uri2602Application implements CommandLineRunner {
 		List<CustomerMinProjection> list = repository.search1("RS");
 		List<CustomerMinDTO> result1 = list.stream().map(CustomerMinDTO::new).collect(Collectors.toList());
 
+		System.out.println("\n****  Resultado SQL Raiz  **** ");
 		result1.forEach(System.out::println);
+		System.out.println("\n\n");
+
+		List<CustomerMinDTO> result2 = repository.search2("RS");
+
+		System.out.println("\n****  Resultado JPQL  **** ");
+		result2.forEach(System.out::println);
+
 	}
 }
--- uri2602/src/main/java/com/devsuperior/uri2602/repository/CustomerRepository.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/repository/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.uri2602.repository;
 
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
 import com.devsuperior.uri2602.projection.CustomerMinProjection;
 import com.devsuperior.uri2602.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +12,9 @@ import java.util.List;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    @Query(nativeQuery = true, value = "select c.name from customers c where c.state = :state")
+    @Query(nativeQuery = true, value = "select c.name from customers c where UPPER(c.state) = UPPER(:state)")
     List<CustomerMinProjection> search1(String state);
+
+    @Query("SELECT new com.devsuperior.uri2602.dto.CustomerMinDTO(c.name) FROM Customer c WHERE UPPER(c.state) = UPPER(:state)")
+    List<CustomerMinDTO> search2(String state);
 }
